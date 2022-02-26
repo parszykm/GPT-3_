3d6ae009e0f89e9448f75c28ced4cb0f8d5a87b2
parszykm <parszyk.m@wp.pl>
2022-02-26 20:45:38 +0100

    commit

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,7 +3,15 @@ import './cta.css'
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div className="gpt3__cta">
+      <div className="gpt3__cta-content">
+        <h4>Request Early Access to Get Started</h4>
+        <h1>Register today & start exploring the endless possiblities.</h1>
+      </div>
+      <div className="gpt3__cta-button">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
   )
 }
 
