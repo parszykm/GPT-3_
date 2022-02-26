f1e14467a5a999f1cffa6b46b071031272bcacb3
parszykm <parszyk.m@wp.pl>
2022-02-26 21:58:23 +0100

    commit

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './article.css'
 
-const Article = () => {
+const Article = ({title, date, imgUrl}) => {
   return (
-    <div>Article</div>
+    <div className="gpt3__article">
+      <div className="gpt3__article-img">
+        <img src={imgUrl} alt="blog_image"></img>
+      </div>
+      <div className="gpt3__article-heading">
+        <div>
+        <p>{date}</p>
+        <h3>{title}</h3>          
+        </div>
+        <p>Read Full Article</p>
+      </div>
+      
+    </div>
   )
 }
 
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Article } from '../../components'
 import './blog.css'
+import {blog01, blog02, blog03, blog04,blog05} from './imports'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className="gpt3__blog section__padding" id="blog">
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">A lot is happening, We are blogging about it.</h1>
+      </div>
+      <div className="gpt3__blog-images"> 
+        <div className="gpt3__blog-images-A">
+          <Article title="GPT-3 and Open  AI is the future. Let us exlore how it is?" date="Sep 26, 2021" imgUrl={blog01} />
+          
+        </div>
+        <div className="gpt3__blog-images-B">
+          <Article title="GPT-3 and Open  AI is the future. Let us exlore how it is?" date="Sep 26, 2021" imgUrl={blog02} />
+          <Article title="GPT-3 and Open  AI is the future. Let us exlore how it is?" date="Sep 26, 2021" imgUrl={blog03} />
+          <Article title="GPT-3 and Open  AI is the future. Let us exlore how it is?" date="Sep 26, 2021" imgUrl={blog04} />
+          <Article title="GPT-3 and Open  AI is the future. Let us exlore how it is?" date="Sep 26, 2021" imgUrl={blog05} />
+
+        </div>
+      </div>
+    </div>
   )
 }
 
